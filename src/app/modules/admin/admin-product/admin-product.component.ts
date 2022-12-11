8c159e209d317fd307e06666a277df421a0b8065
Chalinator <pgkriss@tlen.pl>
2022-12-11 17:24:34 +0100

    wyświetlanie obrazka na liście produktów w panelu administracyjnym

--- src/app/modules/admin/admin-product/admin-product.component.ts
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -7,52 +7,53 @@ import {AdminConfirmDialogService} from "../admin-confirm-dialog.service";
 import {MatTable} from "@angular/material/table";
 
 @Component({
-        selector: 'app-admin-products',
-        templateUrl: './admin-product.component.html',
-        styleUrls: ['./admin-product.component.scss']
+    selector: 'app-admin-products',
+    templateUrl: './admin-product.component.html',
+    styleUrls: ['./admin-product.component.scss']
 })
 export class AdminProductsComponent implements AfterViewInit {
 
-        @ViewChild(MatPaginator) paginator!: MatPaginator;
-        @ViewChild(MatTable) table!: MatTable<any>;
-        displayedColumns: string[] = ["id", "name", "price", "actions"];
-        totalElements: number = 0;
-        dataSource: AdminProduct[] = [];
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatTable) table!: MatTable<any>;
+    displayedColumns: string[] = ["image", "id", "name", "price", "actions"];
+    totalElements: number = 0;
+    dataSource: AdminProduct[] = [];
 
-        constructor(private adminProductService: AdminProductService,
-                    private dialogService: AdminConfirmDialogService
-        ) {
-        }
+    constructor(private adminProductService: AdminProductService,
+                private dialogService: AdminConfirmDialogService
+    ) {
 
-        ngAfterViewInit(): void {
-                this.paginator.page.pipe( // pipe coś jak Observable, ale pozwala na nim używać dodatkowych operatorów
-                          startWith({}),
-                          switchMap(() => {
-                                  return this.adminProductService.getProducts(this.paginator.pageIndex, this.paginator.pageSize);
-                          }),
-                ).subscribe(data => {
-                        this.totalElements = data.totalElements;
-                        this.dataSource = data.content;
-                }) // teraz zaczeło działać stronicowanie
-        }
+    }
 
-        confirmDelete(element: AdminProduct) {
-                this.dialogService.openConfirmDialog("Czy na pewno chcesz usunąć produkt?")
-                          .afterClosed()
-                          .subscribe(result => {
-                                  if (result) {
-                                          this.adminProductService.delete(element.id) // z elementu wyciągam id
-                                                    .subscribe(() => {
-                                                            this.dataSource.forEach((value, index) => { // watość elementu i indeks elementu z tablicy
-                                                                    if (element == value) { // element, który mam, przyrównuję do każdego kolejnego elementu
-                                                                            this.dataSource.splice(index, 1) // 1 - ilość elementów do usunięcia
-                                                                            this.table.renderRows(); // jeśli usuwamy dane z tablicy, trzeba zrobić renderowanie wierszy
-                                                                    }
-                                                            })
-                                                    })
-                                          ;
-                                  }
-                          })
-                ;
-        }
+    ngAfterViewInit(): void {
+        this.paginator.page.pipe( // pipe coś jak Observable, ale pozwala na nim używać dodatkowych operatorów
+            startWith({}),
+            switchMap(() => {
+                return this.adminProductService.getProducts(this.paginator.pageIndex, this.paginator.pageSize);
+            }),
+        ).subscribe(data => {
+            this.totalElements = data.totalElements;
+            this.dataSource = data.content;
+        }) // teraz zaczeło działać stronicowanie
+    }
+
+    confirmDelete(element: AdminProduct) {
+        this.dialogService.openConfirmDialog("Czy na pewno chcesz usunąć produkt?")
+            .afterClosed()
+            .subscribe(result => {
+                if (result) {
+                    this.adminProductService.delete(element.id) // z elementu wyciągam id
+                        .subscribe(() => {
+                            this.dataSource.forEach((value, index) => { // watość elementu i indeks elementu z tablicy
+                                if (element == value) { // element, który mam, przyrównuję do każdego kolejnego elementu
+                                    this.dataSource.splice(index, 1) // 1 - ilość elementów do usunięcia
+                                    this.table.renderRows(); // jeśli usuwamy dane z tablicy, trzeba zrobić renderowanie wierszy
+                                }
+                            })
+                        })
+                    ;
+                }
+            })
+        ;
+    }
 }
