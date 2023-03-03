ba32dca5723f039c4fe4f449916336bd124f8505
Chalinator <pgkriss@tlen.pl>
2023-03-03 14:16:08 +0100

    profil użytkownika

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from "./profile.service";
+import { OrderListDto } from "./model/orderListDto";
+import { JwtService } from "../common/service/jwt.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  orders!: Array<OrderListDto>;
+  displayedColumns = ["id", "placeDate", "orderStatus", "grossValue"];
+
+  constructor(private profileService: ProfileService,
+              // 59.0
+              private jwtService: JwtService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    // 59.1 jeśli nie jest zalogowany to przekirowuję do formularza logowania:
+    if (!this.jwtService.isLoggedIn()) {
+      this.router.navigate(["/login"]);
+    }
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.profileService.getOrders()
+            .subscribe(orders => this.orders = orders)
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from "ngx-cookie-service";
 import { HeaderService } from "./header.service";
 import { CartIconService } from "../../../modules/common/service/cart-icon.service";
+import { JwtService } from "../../../modules/common/service/jwt.service";
 
 @Component({
   selector: 'app-header',
@@ -11,28 +12,28 @@ import { CartIconService } from "../../../modules/common/service/cart-icon.servi
 export class HeaderComponent implements OnInit {
 
   title = "Stonka";
-  // 19.0 dodaję pole do zliczania rzeczy w koszyku:
   cartProductCounter = "";
+  // 57.1 dodaję flagę
+  isLoggedIn = false;
 
-  // 20.1 wstrzykuję coookie i serwis:
   constructor(private cookieService: CookieService,
               private headerService: HeaderService,
-              // 21.2 wstrzykuję serwis:
-              private cartIconService: CartIconService
+              private cartIconService: CartIconService,
+              // 57.0 wstrzykuję jwt serwis:
+              private jwtService: JwtService
   ) { }
 
   ngOnInit(): void {
-   // 20.3 uruchamiam metodę:
     this.getCountProducts();
-    // 21.3 muszę się zasubskrybować na ten serwis:
     this.cartIconService.subject
             .subscribe(counter => this.cartProductCounter = String(counter > 0 ? counter : ""));
+    // 57.2 dodaję tutaj:
+    this.isLoggedIn = this.jwtService.isLoggedIn();
   }
 
-  // 20.2 dodaję metodę:
   getCountProducts() {
     return this.headerService.getCountProducts(Number(this.cookieService.get("cartId"))) // pobieram cookie z serwisu
-            .subscribe(counter => this.cartProductCounter = String(counter > 0 ? counter : "")); // jesli pusto,
-    // to nie pokazuję nic przy ikonce, dlatego > 0
+            .subscribe(
+                    counter => this.cartProductCounter = String(counter > 0 ? counter : "")); // jesli pusto, to nie pokazuję nic przy ikonce, dlatego > 0
   }
 }
